1e9e7c2efb9ade261e9e1baa4dbce877e5801650
Jack Daniels <djpomidor@gmail.com>
2021-04-05 16:41:55 +0300

    just apdate

--- auctions/models.py
+++ auctions/models.py
@@ -22,4 +22,4 @@ class Bid(models.Model):
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
-    text = models.CharField(max_length=64)
+    text = models.CharField(max_length=128)
